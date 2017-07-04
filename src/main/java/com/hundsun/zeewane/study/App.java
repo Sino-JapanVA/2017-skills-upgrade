53fe1ed85729d97a47d3bf24a1389fe157e54fdc
zeewane <zeewane@qq.com>
2017-07-04 16:42:38 +0800

    :confetti_ball: Upload the project.

---
+++ src/main/java/com/hundsun/zeewane/study/App.java
@@ -0,0 +1,11 @@
+package com.hundsun.zeewane.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
