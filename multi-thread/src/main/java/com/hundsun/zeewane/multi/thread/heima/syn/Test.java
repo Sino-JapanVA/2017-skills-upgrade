ebf9bf8b462fc2d17c7db362848a527a09992b8f
zeewane <zeewane@qq.com>
2017-07-06 20:11:59 +0800

    多线程用catch来避免停止

--- multi-thread/src/main/java/com/hundsun/zeewane/multi/thread/heima/syn/Test.java
+++ multi-thread/src/main/java/com/hundsun/zeewane/multi/thread/heima/syn/Test.java
@@ -1,11 +1,34 @@
 package com.hundsun.zeewane.multi.thread.heima.syn;
 
 /**
- * 
+ * 成功验证，只要catch住异常，线程不会挂
  *
  * @author zeewane
  * @date 2017年7月5日 上午9:50:11
  */
 public class Test {
 
+    public static void main(String[] args) {
+        new Thread(new thread()).start();
+    }
+
+    static class thread implements Runnable {
+        public void run() {
+            int index = 10;
+            while (true) {
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                index--;
+                try {
+                    System.out.println(100 / index);
+                } catch (Exception e) {
+                    System.out.println("抓住一个异常" + e);
+                }
+            }
+        }
+    }
 }
