909b467edb155efd863e9d5898ba65f706245775
zeewane <zeewane@qq.com>
2017-07-14 15:44:09 +0800

    Move to latitude.

--- src/main/java/com/hundsun/zeewane/study/App.java
+++ src/main/java/com/hundsun/zeewane/study/App.java
@@ -7,5 +7,6 @@ package com.hundsun.zeewane.study;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("come from latitude");
     }
 }
